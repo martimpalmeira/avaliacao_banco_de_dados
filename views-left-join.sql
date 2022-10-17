97937f08677cefb896475b69c8cbbb1b6d03d7e6
martimpalmeira <martim.desenvolvedor@gmail.com>
2022-10-17 10:40:57 -0300

    Adding view with left join

---
+++ views-left-join.sql
@@ -0,0 +1,9 @@
+CREATE VIEW professores_quant_alunos AS 
+SELECT professores.Nome_Professor, COUNT(alunos.Codigo_Aluno) FROM professores
+LEFT JOIN materias ON professores.Codigo_Professor = materias.Codigo_Professor
+LEFT JOIN alunos_turmas ON materias.Codigo_Materia = alunos_turmas.Codigo_Materia
+LEFT JOIN alunos ON alunos_turmas.Codigo_Aluno = alunos.Codigo_Aluno
+GROUP BY professores.Codigo_Professor;
+
+SELECT * FROM professores_quant_alunos;
+
