cfaf4a97f8a14d6f53f51de2ae38cd9ea46e8354
martimpalmeira <martim.desenvolvedor@gmail.com>
2022-10-17 09:01:33 -0300

    Creating tables

--- avaliacao_banco_de_dados.sql
+++ avaliacao_banco_de_dados.sql
@@ -1,2 +1,58 @@
 CREATE DATABASE avaliacao_banco_de_dados;
 
+CREATE TABLE professores(
+	Codigo_Professor INT PRIMARY KEY AUTO_INCREMENT,
+    Nome_Professor VARCHAR(20)
+);
+
+CREATE TABLE alunos(
+	Codigo_Aluno INT PRIMARY KEY AUTO_INCREMENT,
+    Nome_Aluno VARCHAR(30)
+);
+
+CREATE TABLE materias(
+	Codigo_Materia INT PRIMARY KEY AUTO_INCREMENT,
+    Codigo_Professor INT,
+    Nome_Materia VARCHAR(20),
+    FOREIGN KEY(Codigo_Professor) REFERENCES professores(Codigo_Professor)
+);
+
+CREATE TABLE turmas(
+	Codigo_Turma INT PRIMARY KEY AUTO_INCREMENT,
+    Nome_Turma VARCHAR(2),
+    Turno_Turma VARCHAR(10)
+);
+
+CREATE TABLE alunos_turmas(
+	Codigo_Aluno INT,
+    Codigo_Materia INT,
+    Codigo_Turma INT,
+    FOREIGN KEY(Codigo_Aluno) REFERENCES alunos(Codigo_Aluno),
+    FOREIGN KEY(Codigo_Materia) REFERENCES materias(Codigo_Materia),
+    FOREIGN KEY(Codigo_Turma) REFERENCES turmas(Codigo_Turma)
+);
+
+CREATE TABLE salas(
+	Codigo_Sala INT PRIMARY KEY AUTO_INCREMENT,
+    Nome_Sala VARCHAR(30)
+);
+
+CREATE TABLE aulas(
+	Codigo_Aula INT PRIMARY KEY AUTO_INCREMENT,
+    Codigo_Turma INT,
+    Codigo_Materia INT,
+    Codigo_Sala INT,
+    Horario_Inicio DATE,
+    Horario_Termino DATE,
+    FOREIGN KEY(Codigo_Materia) REFERENCES materias(Codigo_Materia),
+    FOREIGN KEY(Codigo_Turma) REFERENCES turmas(Codigo_Turma),
+    FOREIGN KEY(Codigo_Sala) REFERENCES turmas(Codigo_Sala)
+    
+);
+
+CREATE TABLE funcionarios (
+	Codigo_Funcionario INT PRIMARY KEY AUTO_INCREMENT,
+    Nome_Funcionario VARCHAR(20),
+    Cargo_Funcionario VARCHAR(30),
+    Salario_Funcionario DOUBLE
+);
